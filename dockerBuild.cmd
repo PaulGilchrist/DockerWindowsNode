01422ca1feb0ba39e6d8925c43b5b0b23a4b2423
Paul Gilchrist <Paul Gilchrist>
2017-01-24 14:08:37 -0500

    Updated docs

--- dockerBuild.cmd
+++ dockerBuild.cmd
@@ -1,11 +1,13 @@
 SET containerFolder=c:\temp\container
 :: Remove the old image if it exists before creating a new image
-docker rmi node:latest
+docker rmi paulgilchrist/node:nano
 ::Create a temporary folder and copy in all the files needed for the new container
 md %containerFolder%
 xcopy "c:\program files\nodejs\*.*" %containerFolder%\nodejs\*.* /s
 copy Dockerfile %containerFolder%\
 :: Build the new container
-docker build -t node:nano %containerFolder%
+docker build -t paulgilchrist/node:nano %containerFolder%
 :: Cleanup the temporary folder
 rd %containerFolder% /S /Q
+::docker login
+::docker push paulgilchrist/node:nano
